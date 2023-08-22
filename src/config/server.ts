300b560c6ffb5612e6e6d0de9be5f25b7cd329d7
Md. Pial Ahamed <root.pial@gmail.com>
2023-08-22 14:22:24 +0600

    Add example for destroying request

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,4 +1,5 @@
 export const ServerConfig = {
   apiPort: 80,
   wsPort: 8080,
+  wsPort2: 8081,
 };
--- src/server/websocket.ts
+++ src/server/websocket.ts
@@ -1,18 +1,39 @@
 import { WebSocketServer, WebSocket, RawData } from "ws";
 import { ServerConfig } from "../config";
+import http from "http";
 
 let wsConnection: WebSocket;
 
 export function websocket() {
   const server = new WebSocketServer({ port: ServerConfig.wsPort });
+  const customServer = new WebSocketServer({
+    server: getCustomServer(),
+  });
 
   server.on("listening", () => {
     console.info("Listening websocket server on PORT: ", ServerConfig.wsPort);
   });
+  customServer.on("listening", () => {
+    console.info(
+      "Listening custom websocket server on PORT: ",
+      ServerConfig.wsPort2
+    );
+  });
   server.on("connection", handleWebSocketConnection);
 }
 
+function getCustomServer() {
+  const httpServer = http.createServer();
+  httpServer.on("upgrade", (_req, socket, _head) => {
+    socket.destroy();
+  });
+
+  httpServer.listen(ServerConfig.wsPort2);
+  return httpServer;
+}
+
 function handleWebSocketConnection(connection: WebSocket) {
+  console.log("Connected", Date());
   wsConnection = connection;
   connection.on("message", handleWebSocketMessage);
 }
